1ec9c17da81cf1336b52db0f5151744508872d61
Victor Amuluche <amulucvc@mail.uc.edu>
2024-09-23 20:02:27 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double doubleprice = 210.50;
+        double doubleTax = 0.05;
+        double doubleTotal = 0;
+    doubleTotal = doubleprice * doubleTax;
+    System.out.print("you total is " + doubleTotal + " the price of the item was " + doubleprice );
+    }
+}
